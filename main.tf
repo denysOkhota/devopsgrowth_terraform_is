113dbce1f63c1cc10ad612b5e006ff8c4e8fcbd5
denysOkhota <qwerty165343@gmail.com>
2023-07-17 00:05:02 +0300

    1 App Service Plan
    1 App Service - Integrate with Vnet
    
    , Enable System Managed Identity
    
    1 App Insights
    
    - Linked to App Service
    
    1 VNET
    
    1 Storage account - Configure Private Endpoint with VNET and
    
    1 Storage account for state
    
    State should be stored in storage account

--- main.tf
+++ main.tf
@@ -30,10 +30,10 @@ resource "azurerm_virtual_network" "vnet" {
 }
 
 resource "azurerm_subnet" "subnet1" {
-  address_prefixes       = ["10.0.0.0/24"]
-  name                   = "subnet1"
-  resource_group_name    = azurerm_resource_group.taskrg.name
-  virtual_network_name   = azurerm_virtual_network.vnet.name
+  address_prefixes                          = ["10.0.0.0/24"]
+  name                                      = "subnet1"
+  resource_group_name                       = azurerm_resource_group.taskrg.name
+  virtual_network_name                      = azurerm_virtual_network.vnet.name
   private_endpoint_network_policies_enabled = true
 
   delegation {
@@ -41,21 +41,32 @@ resource "azurerm_subnet" "subnet1" {
 
     service_delegation {
       name = "Microsoft.Web/serverFarms"
-      actions = [ "Microsoft.Network/virtualNetworks/subnets/join/action",
-      "Microsoft.Network/virtualNetworks/subnets/prepareNetworkPolicies/action",
-      "Microsoft.Network/virtualNetworks/subnets/unprepareNetworkPolicies/action" ]
+      actions = ["Microsoft.Network/virtualNetworks/subnets/join/action",
+        "Microsoft.Network/virtualNetworks/subnets/prepareNetworkPolicies/action",
+      "Microsoft.Network/virtualNetworks/subnets/unprepareNetworkPolicies/action"]
     }
   }
 
-  
+
 }
 
+resource "azurerm_subnet" "subnet2" {
+  address_prefixes                          = ["10.0.1.0/24"]
+  name                                      = "subnet2"
+  resource_group_name                       = azurerm_resource_group.taskrg.name
+  virtual_network_name                      = azurerm_virtual_network.vnet.name
+  private_endpoint_network_policies_enabled = true
+}
+
+data "azurerm_subscription" "current" {}
+
 resource "azurerm_service_plan" "serviceplan" {
-  location             = azurerm_resource_group.taskrg.location
-  name                 = "dendevopsgrowth_sp"
-  os_type              = "Linux"
-  sku_name             = "B1"
-  resource_group_name  = azurerm_resource_group.taskrg.name
+  location            = azurerm_resource_group.taskrg.location
+  name                = "dendevopsgrowth_sp"
+  os_type             = "Linux"
+  sku_name            = "B1"
+  resource_group_name = azurerm_resource_group.taskrg.name
+  
 }
 
 resource "azurerm_linux_web_app" "webapp" {
@@ -64,19 +75,34 @@ resource "azurerm_linux_web_app" "webapp" {
   location                  = azurerm_resource_group.taskrg.location
   service_plan_id           = azurerm_service_plan.serviceplan.id
   virtual_network_subnet_id = azurerm_subnet.subnet1.id
-
+  
+  identity {
+    type = "SystemAssigned"
+  }
   site_config {}
+  app_settings = {
+    APPINSIGHTS_INSTRUMENTATIONKEY = "${azurerm_application_insights.appins.instrumentation_key}"
+  }
 }
 
 resource "azurerm_application_insights" "appins" {
-  name                 = "web_app_insights"
-  application_type     = "web"
-  location             = azurerm_linux_web_app.webapp.location
-  resource_group_name  = azurerm_resource_group.taskrg.name
+  name                = "web_app_insights"
+  application_type    = "web"
+  location            = azurerm_resource_group.taskrg.location
+  resource_group_name = azurerm_resource_group.taskrg.name
+}
+
+output "instrumentation_key" {
+  value = azurerm_application_insights.appins.instrumentation_key
+  sensitive = true
+}
+
+output "app_id" {
+  value = azurerm_application_insights.appins.app_id
 }
 
 resource "azurerm_private_dns_zone" "dns-zone" {
-  name                = "privatelink.blob.core.windows.net"
+  name                = "privatelink.file.core.windows.net"
   resource_group_name = azurerm_resource_group.taskrg.name
 }
 
@@ -96,29 +122,31 @@ resource "azurerm_storage_account" "storage_account_1" {
 }
 
 resource "azurerm_storage_share" "storageshare" {
-    name = "dendevopsgrowthshare"
-    storage_account_name = azurerm_storage_account.storage_account_1.name
-    quota = 500
+  name                 = "dendevopsgrowthshare"
+  storage_account_name = azurerm_storage_account.storage_account_1.name
+  quota                = 500
 }
 
 resource "azurerm_private_endpoint" "privateendpoint" {
-    name = "dendevopsgrowthpe"
-    resource_group_name = azurerm_resource_group.taskrg.name
-    location = azurerm_resource_group.taskrg.location
-    subnet_id = azurerm_subnet.subnet1.id
+  name                = "dendevopsgrowthpe"
+  resource_group_name = azurerm_resource_group.taskrg.name
+  location            = azurerm_resource_group.taskrg.location
+  subnet_id           = azurerm_subnet.subnet2.id
 
-    private_service_connection {
-      name = "dendevopsgrowth_psc"
-      is_manual_connection = false
-      private_connection_resource_id = azurerm_storage_account.storage_account_1.id
-      subresource_names = [ "file" ]
-    }
+  private_service_connection {
+    name                           = "dendevopsgrowth_psc"
+    is_manual_connection           = false
+    private_connection_resource_id = azurerm_storage_account.storage_account_1.id
+    subresource_names              = ["file"]
+  }
 }
 
-resource "azurerm_private_dns_a_record" "dns_a" {
-  name                = "dendevopsgrowth"
-  zone_name           = azurerm_private_dns_zone.dns-zone.name
-  resource_group_name = azurerm_resource_group.network-rg.name
-  ttl                 = 300
-  records             = [azurerm_private_endpoint.]
-}
+
+
+# resource "azurerm_private_dns_a_record" "dns_a" {
+#   name                = "dendevopsgrowth"
+#   zone_name           = azurerm_private_dns_zone.dns-zone.name
+#   resource_group_name = azurerm_resource_group.network-rg.name
+#   ttl                 = 300
+#   records             = []
+# }
