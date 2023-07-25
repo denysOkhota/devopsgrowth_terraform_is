edbdcd49b4875d92827d62fb18191cbd163c3767
denysOkhota <qwerty165343@gmail.com>
2023-07-25 16:25:41 +0300

    updated key_vault

--- modules/key_vault/main.tf
+++ modules/key_vault/main.tf
@@ -4,7 +4,7 @@ resource "azurerm_key_vault" "keyvault" {
   resource_group_name = var.resource_group_name
   sku_name            = var.sku_name
   tenant_id           = var.tenant_id
-
+  soft_delete_retention_days = 12
   access_policy {
     tenant_id = var.tenant_id
     object_id = var.object_id
